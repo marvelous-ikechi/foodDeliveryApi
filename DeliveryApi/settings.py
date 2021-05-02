ad2dc5027a091a7956777fa6fc815a2c0d74b1d9
BLESSING <blessing@BLESSINGs-MacBook-Pro.local>
2021-05-02 13:52:26 +0100

    heroku setup completed

--- DeliveryApi/settings.py
+++ DeliveryApi/settings.py
@@ -97,8 +97,7 @@ else:
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
