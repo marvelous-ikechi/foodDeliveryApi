cf16f8c6252d73ce495e49de00d819cc6d7e784e
BLESSING <blessing@BLESSINGs-MacBook-Pro.local>
2021-05-02 21:41:29 +0100

    database changed to sqlite

--- DeliveryApi/settings.py
+++ DeliveryApi/settings.py
@@ -73,26 +73,34 @@ ROOT_URLCONF = 'DeliveryApi.urls'
 
 DATABASE_URL = os.getenv('DATABASE_URL', None)
 
-if not DATABASE_URL:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    db_info = urlparse(DATABASE_URL)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'db',
-            'USER': db_info.username,
-            'PASSWORD': db_info.password,
-            'HOST': db_info.hostname,
-            'PORT': db_info.port,
-            'OPTIONS': {'sslmode': 'require'},
-        }
-    }
+
+# if not DATABASE_URL:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     db_info = urlparse(DATABASE_URL)
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'db',
+#             'USER': db_info.username,
+#             'PASSWORD': db_info.password,
+#             'HOST': db_info.hostname,
+#             'PORT': db_info.port,
+#             'OPTIONS': {'sslmode': 'require'},
+#         }
+#     }
 
 TEMPLATES = [
     {
